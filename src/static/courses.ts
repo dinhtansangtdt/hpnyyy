2a7a305e9afe2764fb18b1b766e624c15560196a
Đinh Tấn Sang <dinhtansang.tdt@gmail.com>
2024-09-17 23:51:17 +0700

    yeh

--- src/static/courses.ts
+++ src/static/courses.ts
@@ -1,23 +1,24 @@
 export default [{ title: 'Sinh hoạt chủ nhiệm', location: 'LT-A15-CS1', week: 1, start: 1, duration: 1, weeks: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17] },
- { title: 'An toàn vệ sinh Lao động ngành điện', location: 'LT-A15-CS1', week: 1, start: 2, duration: 3, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17] },
- {title: 'Khí cụ điện', location: 'LT-B32-CS1', week: 1, start: 6, duration: 4, weeks: [ 11, 12, 13, 14, 15, 16, 17] },
+ { title: 'An toàn vệ sinh Lao động ngành điện', location: 'LT-A15-CS1', week: 1, start: 2, duration: 3, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] },
+ {title: 'Khí cụ điện', location: 'LT-B32-CS1', week: 1, start: 6, duration: 4, weeks: [ 12, 13, 14, 15, 16] },
  {title: 'Giáo dục thể chất', location: 'Sân trường-CS1', week: 1, start: 7, duration: 4, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] },
 
- {title: 'Tiếng anh', location: 'LT-C107-CS4', week: 2, start: 1, duration: 4, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17] },
+ {title: 'Tiếng anh', location: 'LT-C107-CS4', week: 2, start: 1, duration: 4, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] },
  {title: 'Mạch điện', location: 'LT-B32-CS1', week: 2, start:6, duration: 4, weeks: [ 5, 6, 7, 8, 9, 10, 11, 12, 13] },
  {title: 'Mạch điện', location: 'LT-B32-CS1', week: 2, start:6, duration: 3, weeks: [ 14] },
 
  {title: 'Giáo dục thể chất', location: 'Sân trường-CS1', week: 3, start: 1, duration: 4, weeks: [11] },
- {title: 'Khí cụ điện', location: 'LT-B32-CS1', week: 3, start: 1, duration: 4, weeks: [14, 15, 16, 17] },
+ {title: 'Khí cụ điện', location: 'LT-B31-CS1', week: 3, start: 1, duration: 4, weeks: [14, 15, 16, 17] },
  {title: 'Pháp Luật', location: 'LT-C107-CS4', week: 3, start: 6, duration: 4, weeks: [ 2, 3, 4, 5, 6, 7, 8] },
  {title: 'Pháp Luật', location: 'LT-C107-CS4', week: 3, start: 6, duration: 4, weeks: [ 9] },
  {title: 'Khí cụ điện', location: 'LT-B32-CS1', week: 3, start: 6, duration: 2, weeks: [10, 11, 12, 13] },
 
- {title: 'Tiếng anh', location: 'LT-C107-CS4', week: 4, start: 1, duration: 4, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17] },
+ {title: 'Tiếng anh', location: 'LT-C107-CS4', week: 4, start: 1, duration: 4, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] },
 
- {title: 'Đo lượng điện - cảm biến', location: 'LT-B21-CS1', week: 5, start: 1, duration: 4, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] },
- {title: 'Đo lượng điện - cảm biến', location: 'LT-B21-CS1', week: 5, start: 1, duration: 1, weeks: [ 13] },
- {title: 'Mạch điện', location: 'LT-B32-CS1', week: 5, start:6, duration: 4, weeks: [ 5, 6, 7, 8, 9, 10, 11, 12] },
+ {title: 'Đo lượng điện - cảm biến', location: 'LT-B31-CS1', week: 5, start: 1, duration: 4, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] },
+ {title: 'Đo lượng điện - cảm biến', location: 'LT-B31-CS1', week: 5, start: 1, duration: 1, weeks: [ 13] },
+ {title: 'Khí cụ điện', location: 'LT-B31-CS1', week: 5, start: 1, duration: 4, weeks: [ 14, 15, 16] },
+ {title: 'Mạch điện', location: 'LT-B32-CS1', week: 5, start:6, duration: 4, weeks: [ 5, 6, 7, 8, 9, 10, 11, 12, 13] },
 
- {title: 'Vẽ thiết kế điện', location: 'TH Tự động hóa 1 - CS1', week: 6, start:6, duration: 4, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17] },
+ {title: 'Vẽ thiết kế điện', location: 'TH Tự động hóa 1 - CS1', week: 6, start:6, duration: 4, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] },
 ]
