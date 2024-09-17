bae9a28bc04012a469b83d38b8d075ac8e750291
Đinh Tấn Sang <dinhtansang.tdt@gmail.com>
2024-09-18 00:34:13 +0700

    aaa

--- src/static/courses.ts
+++ src/static/courses.ts
@@ -20,5 +20,5 @@ export default [{ title: 'Sinh hoạt chủ nhiệm', location: 'LT-A15-CS1', we
  {title: 'Khí cụ điện', location: 'LT-B31-CS1', week: 5, start: 1, duration: 4, weeks: [ 14, 15, 16] },
  {title: 'Mạch điện', location: 'LT-B32-CS1', week: 5, start:6, duration: 4, weeks: [ 5, 6, 7, 8, 9, 10, 11, 12, 13] },
 
- {title: 'Vẽ thiết kế điện', location: 'TH Tự động hóa 1 - CS1', week: 6, start:6, duration: 4, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] },
+ {title: 'Vẽ thiết kế điện', location: 'TH Tự động hóa 1 - CS1', week: 6, start:6, duration: 4, weeks: [ 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] },
 ]
